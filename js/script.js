2af6fa2a778b4a0c395c4dd26b07264cb62fb778
Vasyl Karakevych <vasyl.karakevych@gmail.com>
2020-06-27 21:43:02 +0300

    2. new object personalMowiesDB

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // 1. How do you saw films
 let numberOfFilms = prompt("How do you saw films?",'');
-console.log(numberOfFilms);
+// console.log(numberOfFilms);
+
+// 2. new object personalMowiesDB
+let personalMoviesDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(personalMoviesDB);
