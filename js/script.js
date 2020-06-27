4301f058f2c8216dbd4edd33adfc32ec2b521359
Vasyl <vasyl.karakevych@gmail.com>
2020-06-27 23:04:18 +0300

    3. two questions !

--- js/script.js
+++ js/script.js
@@ -10,15 +10,4 @@ let personalMoviesDB =  {
     genres: [],
     privat: false
 };
-// console.log(personalMoviesDB);
-
-//  3. two questions
-let nameOfFilm = prompt("Write last name films which your saw",'James Bond'),
-    ratingOfFilm = prompt("How rating?",'2.1'),
-    nameOfFilm2 = prompt("Write last name films which your saw",'James Bond'),
-    ratingOfFilm2 = prompt("How rating?",'2.1');
-
- personalMoviesDB.movies[nameOfFilm] = ratingOfFilm;
- personalMoviesDB.movies[nameOfFilm2] = ratingOfFilm2;
-
 console.log(personalMoviesDB);
